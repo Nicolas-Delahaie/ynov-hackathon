9da3e70c3eca7ce43d6064459f6e915608e97765
NathanhYnov <nathan.hullo@ynov.com>
2025-01-23 15:51:28 +0100

    Converted seismometer value to percent

--- seismometer.py
+++ seismometer.py
@@ -1,11 +1,12 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
-#import webrepl
-#webrepl.start()
+# import esp
+# esp.osdebug(None)
+# import webrepl
+# webrepl.start()
 from math import sqrt
 from machine import Pin, SoftI2C
 from time import sleep_ms, sleep
+
 
 def signedIntFromBytes(x, endian="big"):
     y = int.from_bytes(x, endian)
@@ -13,6 +14,7 @@ def signedIntFromBytes(x, endian="big"):
         return -((65535 - y) + 1)
     else:
         return y
+
 
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000)
 
@@ -26,18 +28,23 @@ while True:
     except Exception as e:
         print("ESP32 could not communicate with module")
         raise e
-    
-    #0x3B
-    
+
+    # 0x3B
+
     data = i2c.readfrom_mem(0x68, 0x3B, 6)
-    
+
     x = signedIntFromBytes(data[0:2]) / 16384.0
     y = signedIntFromBytes(data[2:4]) / 16384.0
     z = signedIntFromBytes(data[4:6]) / 16384.0
     d = {"x": x, "y": y, "z": z}
-    
-    res = (sqrt(d["x"]**2+d["y"]**2+d["z"]**2) - 1) / 1.24
+
+    res = (sqrt(d["x"] ** 2 + d["y"] ** 2 + d["z"] ** 2) - 1) / 1.24
+
+    if res < 0:
+        res = 0
+    if res > 1:
+        res = 1
 
     print(res)
-    
+
     sleep(1)
